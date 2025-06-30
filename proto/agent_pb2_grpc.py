32fad64de1cffe74cbc8ac30831f79ab49615cf2
Tyler Talbert <tylertalbert02@gmail.com>
2025-06-30 17:22:33 -0400

    fix(proto): Introduces fix for AgentServiceStub

--- proto/agent_pb2_grpc.py
+++ proto/agent_pb2_grpc.py
@@ -1,23 +1,22 @@
 import grpc
-
 from . import agent_pb2 as agent__pb2
 
 class AgentServiceStub(object):
-    '/agent.AgentService/RunTask',
-    request_serializer = agent__pb2.TaskRequest.SerializeToString,
-    response_deserializer = agent__pb2.TaskReply.FromString,
-
-)
+    def __init__(self, channel):
+        self.RunTask = channel.unary_unary(
+            '/agent.AgentService/RunTask',
+            request_serializer=agent__pb2.TaskRequest.SerializeToString,
+            response_deserializer=agent__pb2.TaskReply.FromString,
+        )
 
 class AgentServiceServicer(object):
-
     def RunTask(self, request, context):
         raise NotImplementedError('Method not implemented!')
 
 
 def add_AgentServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'RunTask': grpc.unary_unary_rpc_method_handler(
+        'RunTask': grpc.unary_unary_rpc_method_handler(
             servicer.RunTask,
             request_deserializer=agent__pb2.TaskRequest.FromString,
             response_serializer=agent__pb2.TaskReply.SerializeToString,
